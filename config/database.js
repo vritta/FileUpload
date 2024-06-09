9b62f00ba189b4987a4a709f86df07dc99b196be
vritta <anuhartripathi15@gmail.com>
2024-06-09 13:56:55 +0530

    installed cloudinary package

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+
 require("dotenv");
+
 dbConnect = ()=>{
-    mongoose.connect(process.env.DATABASE_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
     .then(()=>{console.log("Database connected successfully")})
     .catch((err)=>{
