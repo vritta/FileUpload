6be2e17f1e8648bbed207158356f84b505461235
vritta <anuhartripathi15@gmail.com>
2024-06-09 12:10:40 +0530

    created database file

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv");
+dbConnect = ()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>{console.log("Database connected successfully")})
+    .catch((err)=>{
+        console.log("Sorry, DB Connection Issue Occured!!!");
+        console.error(err);
+        process.exit(1);
+    });
+}
+
+module.exports = dbConnect;
